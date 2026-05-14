bdf0d2ab2153dc0af99974fb4a24da88b47fb446
Saini-Bharat <bharatskataria@gmail.com>
2026-05-14 22:41:02 +0530

    Add: intersection-of-two-arrays - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    intersection(nums1, nums2) {
+        let map = new Set()
+        let common = new Set()
+        for(let i = 0 ; i < nums1.length; ++i){
+            map.add(nums1[i]);
+        }
+        for(let i = 0 ; i < nums2.length ; ++i){
+            if(map.has(nums2[i])){
+                common.add(nums2[i])
+            }
+        }
+        return [...common]
+    }
+}
