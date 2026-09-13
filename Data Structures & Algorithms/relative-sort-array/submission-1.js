8b88cf8d610a01bb5bb8b9ea619ecf27a1ec84c7
Saini-Bharat <bharatskataria@gmail.com>
2026-09-14 03:38:21 +0530

    Add: relative-sort-array - submission-1

---
+++ Data Structures & Algorithms/relative-sort-array/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} arr1
+     * @param {number[]} arr2
+     * @return {number[]}
+     */
+    relativeSortArray(arr1, arr2) {
+        let map = {}
+        let res = []
+        let remaning = []
+        
+        for(let i = 0 ; i < arr1.length ; ++i){
+            map[arr1[i]] = (map[arr1[i]] || 0) + 1;
+            if(!arr2.includes(arr1[i])){
+                remaning.push(arr1[i])
+            }
+        }
+
+        for(let i = 0 ; i < arr2.length ; ++i){
+                for(let j = 0 ; j < map[arr2[i]] ; ++j){
+                    res.push(arr2[i]);
+                }
+        }
+
+        remaning.sort((a,b) => a-b);
+        res.push(...remaning)
+        return res
+    }
+}
